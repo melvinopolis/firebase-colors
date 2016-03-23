4e261866dba4cad4c6464e5861a648999aad1382
Mariel Van Norman <mariel.van.norman@levvel.io>
2016-03-22 20:45:36 -0400

    updates to add functionality of save button

--- app/index.jsx
+++ app/index.jsx
@@ -23,15 +23,32 @@ class App extends React.Component {
 	}
 
 	colorChange(event) {
+		//for live change to local and firebase
 		this.ref.update({
 			color: event.target.value
 		})
+		//for change to firebase on save (local state is still live)
+		// this.setState({
+		// 	color: event.target.value
+		// })
 	}
 
 	backgroundChange(event) {
+		//for live change to local and firebase
 		this.ref.update({
 			backgroundColor: event.target.value
 		})
+		//for change to firebase on save (local state is still live)
+		// this.setState({
+		// 	backgroundColor: event.target.value
+		// })
+	}
+
+	save() {
+		// this.ref.set({
+		// 	color: this.state.color,
+		// 	backgroundColor: this.state.backgroundColor
+		// })
 	}
 
 	render() {
@@ -44,11 +61,16 @@ class App extends React.Component {
 			color: this.state.color,
 			backgroundColor: this.state.backgroundColor
 		}
+
 		return (
 			<div>
 				<form>
-					<input type="text" value={this.state.color} onChange={this.colorChange.bind(this)} />
-					<input type="text" value={this.state.backgroundColor} onChange={this.backgroundChange.bind(this)} />
+					<label htmlFor="color">font color: </label>
+					<input type="text" id="color" value={this.state.color} onChange={this.colorChange.bind(this)} />
+					<br />
+					<label htmlFor="backgroundColor">background color: </label>
+					<input type="text" id="backgroundColor" value={this.state.backgroundColor} onChange={this.backgroundChange.bind(this)} />
+					{/* <button onClick={this.save.bind(this)}>save</button> */}
 				</form>
 
 				<div style={divStyles}>
